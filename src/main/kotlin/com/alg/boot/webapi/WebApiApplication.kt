cf5096de5ac14415752154bedfe6f79dca1b2291
Arturo López <lgzarturo@gmail.com>
2022-01-08 04:50:54 -0500

    start: commit inicial

---
+++ src/main/kotlin/com/alg/boot/webapi/WebApiApplication.kt
@@ -0,0 +1,11 @@
+package com.alg.boot.webapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<WebApiApplication>(*args)
+}

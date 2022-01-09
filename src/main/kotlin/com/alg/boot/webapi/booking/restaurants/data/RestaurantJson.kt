6076f5cde387cc92dbaf62744bcd2aa63157ae30
Arturo López <lgzarturo@gmail.com>
2022-01-09 04:21:23 -0500

    test: obtener el listado de restaurantes

--- src/main/kotlin/com/alg/boot/webapi/booking/restaurants/data/RestaurantJson.kt
+++ src/main/kotlin/com/alg/boot/webapi/booking/restaurants/data/RestaurantJson.kt
@@ -4,16 +4,16 @@ import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-class RestaurantJson {
+class RestaurantJson(
     @JsonProperty("id")
-    var id: Long? = null
+    var id: Long? = null,
 
     @JsonProperty("address")
-    var address: String? = null
+    var address: String? = null,
 
     @JsonProperty("description")
-    var description: String? = null
+    var description: String? = null,
 
     @JsonProperty("image")
     var image: String? = null
-}
+)
--- src/test/kotlin/com/alg/boot/webapi/booking/v1/RestaurantControllerTest.kt
+++ src/test/kotlin/com/alg/boot/webapi/booking/v1/RestaurantControllerTest.kt
@@ -1,12 +1,14 @@
 package com.alg.boot.webapi.booking.v1
 
 import com.alg.boot.webapi.booking.restaurants.data.RestaurantDetailJson
+import com.alg.boot.webapi.booking.restaurants.data.RestaurantJson
 import com.alg.boot.webapi.booking.restaurants.services.RestaurantService
 import com.alg.boot.webapi.booking.turns.data.TurnJson
 import com.alg.boot.webapi.handlers.exceptions.GeneralException
 import com.alg.boot.webapi.handlers.responses.GeneralResponse
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.mockito.InjectMocks
 import org.mockito.Mock
@@ -25,9 +27,11 @@ internal class RestaurantControllerTest {
     fun setUp() {
         MockitoAnnotations.openMocks(this)
         Mockito.`when`(restaurantService.getRestaurant(1)).thenReturn(RESTAURANT_DETAIL_JSON)
+        Mockito.`when`(restaurantService.getRestaurants()).thenReturn(RESTAURANT_JSON_LIST)
     }
 
     @Test
+    @DisplayName("Get restaurant detail by id")
     @Throws(GeneralException::class)
     fun getRestaurantTest() {
         val response: GeneralResponse<RestaurantDetailJson> = restaurantController.getRestaurant(RESTAURANT_ID)
@@ -35,6 +39,17 @@ internal class RestaurantControllerTest {
         Assertions.assertEquals(SUCCESS_CODE, response.code)
         Assertions.assertEquals(SUCCESS_MESSAGE, response.message)
         Assertions.assertEquals(RESTAURANT_DETAIL_JSON, response.data)
+    }
+
+    @Test
+    @DisplayName("Get restaurants")
+    @Throws(GeneralException::class)
+    fun getRestaurantsTest() {
+        val response: GeneralResponse<List<RestaurantJson>> = restaurantController.getRestaurants()
+        Assertions.assertEquals(SUCCESS_STATUS, response.status)
+        Assertions.assertEquals(SUCCESS_CODE, response.code)
+        Assertions.assertEquals(SUCCESS_MESSAGE, response.message)
+        Assertions.assertEquals(RESTAURANT_JSON_LIST, response.data)
     }
 
     companion object {
@@ -58,6 +73,20 @@ internal class RestaurantControllerTest {
             description = "Description 1",
             image = "Image 1",
             turns = TURN_LIST_JSON
+        )
+        private val RESTAURANT_JSON_LIST: List<RestaurantJson> = listOf(
+            RestaurantJson(
+                id = 1L,
+                address = "Address 1",
+                description = "Description 1",
+                image = "Image 1"
+            ),
+            RestaurantJson(
+                id = 2L,
+                address = "Address 2",
+                description = "Description 2",
+                image = "Image 2"
+            )
         )
     }
 }
